3bf0705b770ab997684a7a5d8f2587235acc2fc0
cesardavid26 <cesardavidcp@ufps.edu.co>
2020-10-16 07:55:21 -0500

    se agrego el JS

---
+++ JS/js.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $("#tecno").click(function () {
+        $('html,body').animate({
+            scrollTop: $("#destino2").offset().top
+        }, 500);
+    });
+    });
+$(document).ready(function() {
+    $("#contact").click(function () {
+        $('html,body').animate({
+            scrollTop: $("#destino3").offset().top
+        }, 500);
+    });
+    });
+$(document).ready(function() {
+    $("#arqui").click(function () {
+        $('html,body').animate({
+            scrollTop: $("#destino1").offset().top
+        }, 500);
+    });
+    });
